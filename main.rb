409d0d359a0229e185c6c2c7e9a74999fd647605
angel23v <angelvazquezvirgen@gmail.com>
2023-12-21 23:20:39 -0600

    Test file

--- main.rb
+++ main.rb
@@ -1,31 +1,72 @@
-# ddd
+# frozen_string_literal: true
 
+# classs
 class Cifrado
-
   attr_accessor(:original_message, :key)
 
-  def initialize(original_message, key)
-    @original_message = original_message.upcase
-    @key = key.upcase
+  def initialize(original_message = 'a', key = 'a')
+    # get the words and make it upcase, after in an array and finally put them together in an array
+    # if there is more than a word.
+    @original_message = original_message.upcase.split.join
+    @key = key.upcase.split.join
     @cifrado = ('A'..'Z').to_a
   end
 
   def encrypt_word(word)
+    # Change type word in a array and get the index of each letter
     word.split('').map do |letter|
       @cifrado.index(letter)
     end
   end
 
-  def call_method
+  def complete_message?
+    # loop over message for concat key letters until get the same lenght of word message
+    message = @original_message
+    key = @key
+
+    return key unless message.length > key.length
+
+    message.each_char.with_index do |_, index|
+      key += key[index]
+    end
+    key
+  end
+
+  def index_sum
+    # get the sum of each index letter
     message = encrypt_word(@original_message)
-    puts ''
-    key = encrypt_word(@key)
+
+    key_complete = complete_message?
+    key_encrypt = encrypt_word(key_complete)
 
     message.map.with_index do |ele, index|
-      ele + key[index]
+      ele + key_encrypt[index]
     end
+  end
+
+  def calculate_correct_index
+    # fix if the sum is bigger than cifrado which is the array with the letters
+    index_sum.map do |sum|
+      if @cifrado.length <= sum
+        sum - @cifrado.length
+      else
+        sum
+      end
+    end
+  end
+
+  def display_encrypt
+    # display the correct encrypt message looping over an array
+    # who has the correct sum and get the letter by the index sum
+    encrypt_message = ''
+
+    calculate_correct_index.each do |idx_sum_ele|
+      encrypt_message += @cifrado[idx_sum_ele]
+    end
+    puts "Encrypt message: #{encrypt_message}"
+    encrypt_message
   end
 end
 
-g = Cifrado.new('dahlia', 'angel')
-g.call_method
+g = Cifrado.new('zk', 'zwy')
+g.display_encrypt
---
+++ test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'main'
+
+# Test for Cifrado de Vigenère:
+class TestCifradoVigenere < Minitest::Test
+  def setup
+    @cifrado_one = Cifrado.new('hola', 'secreto')
+    @cifrado_key = Cifrado.new('angel', 'celular')
+    @cifrado_msg = Cifrado.new('rubyonrails', 'gema')
+    @cifrado_word = Cifrado.new('java script', 'node js')
+  end
+
+  def test_encrypt_word
+    assert_equal 'ZSNR', @cifrado_one.display_encrypt
+  end
+
+  def test_key_bigger_than_msg
+    assert_equal 'CRRYW', @cifrado_key.display_encrypt
+  end
+
+  def test_key_less_than_msg
+    assert_equal 'XYNYURDAOPE', @cifrado_msg.display_encrypt
+  end
+
+  def test_more_than_one_word
+    assert_equal 'WOYEBUEWSX', @cifrado_word.display_encrypt
+  end
+end
